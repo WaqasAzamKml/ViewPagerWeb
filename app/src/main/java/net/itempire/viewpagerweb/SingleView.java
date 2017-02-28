1d1307262b8a7f91117f8834694289f715f4d379
Muhammad Waqas <waqasazamkml@gmail.com>
2017-02-28 16:08:08 +0500

    Added Universal Image Loader library to load images form web, on creation of fragment. Added ProgressBar over the image so that when image is not loaded, progressbar is shown to user.

--- app/src/main/java/net/itempire/viewpagerweb/SingleView.java
+++ app/src/main/java/net/itempire/viewpagerweb/SingleView.java
@@ -1,18 +1,69 @@
 package net.itempire.viewpagerweb;
 
+import android.app.Dialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 public class SingleView extends Fragment {
+
+    ImageView imgPetPhoto;
+    ProgressBar pbImage;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_single_view, container, false);
+        View v = inflater.inflate(R.layout.fragment_single_view, container, false);
+
+        imgPetPhoto = (ImageView) v.findViewById(R.id.imgPetPhoto);
+        pbImage = (ProgressBar) v.findViewById(R.id.pbImage);
+
+
+
+        String imgURL = "https://www.pakpets.com/uploads/ad_19393335700762.jpg";
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true).cacheOnDisk(true).build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions).build();
+        ImageLoader.getInstance().init(config); // Do it on Application start
+
+        // Then later, when you want to display image
+        ImageLoader.getInstance().displayImage(imgURL, imgPetPhoto, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                pbImage.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                pbImage.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                pbImage.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                pbImage.setVisibility(View.GONE);
+            }
+        }); // Default options will be used
+        return v;
     }
 
 }
