f2504f17b33380bb3affcaf1532c43c2fd9d9a1f
Muhammad Waqas <waqasazamkml@gmail.com>
2017-03-02 13:08:57 +0500

    Implemented ViewPager on Pet Images also. Created custom ImageSwipeLayout and ImageSwipeAdapter for this.

---
+++ app/src/main/java/net/itempire/viewpagerweb/ImageSwipeAdapter.java
@@ -0,0 +1,93 @@
+package net.itempire.viewpagerweb;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+/**
+ * Created by MIRSAAB on 3/1/2017.
+ */
+
+public class ImageSwipeAdapter extends PagerAdapter {
+    String[] imageURIStrings = {"https://www.pakpets.com/uploads/ad_19393335700762.jpg",
+            "https://www.pakpets.com/uploads/ad_19663014620032.jpg",
+            "https://www.pakpets.com/uploads/ad_19653112584040.jpg"};
+    //int[] imageResources = {R.drawable.pet_image_1, R.drawable.pet_image_2, R.drawable.pet_image_3};
+    Context context;
+    LayoutInflater layoutInflater;
+    ImageView imgPet;
+    ProgressBar progressBar;
+
+    public ImageSwipeAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imageURIStrings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return (view == (FrameLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = layoutInflater.inflate(R.layout.image_swipe_layout, container, false);
+        imgPet = (ImageView) v.findViewById(R.id.imgPetPhoto);
+        progressBar = (ProgressBar) v.findViewById(R.id.pbImage);
+        //imgPet.setImageResource(imageResources[position]);
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true).cacheOnDisk(true).build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .defaultDisplayImageOptions(defaultOptions).build();
+        ImageLoader.getInstance().init(config); // Do it on Application start
+
+        String imgURL = imageURIStrings[position];
+        // Then later, when you want to display image
+        ImageLoader.getInstance().displayImage(imgURL, imgPet, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+        container.addView(v);
+        return v;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((FrameLayout)object);
+    }
+}
--- app/src/main/java/net/itempire/viewpagerweb/SingleView.java
+++ app/src/main/java/net/itempire/viewpagerweb/SingleView.java
@@ -1,25 +1,20 @@
 package net.itempire.viewpagerweb;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
-
 public class SingleView extends Fragment {
 
     ImageView imgPetPhoto;
     ProgressBar pbImage;
+    ViewPager viewPager;
+    ImageSwipeAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,51 +22,54 @@ public class SingleView extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_single_view, container, false);
 
-        imgPetPhoto = (ImageView) v.findViewById(R.id.imgPetPhoto);
-        pbImage = (ProgressBar) v.findViewById(R.id.pbImage);
+//        imgPetPhoto = (ImageView) v.findViewById(R.id.imgPetPhoto);
+//        pbImage = (ProgressBar) v.findViewById(R.id.pbImage);
+        viewPager = (ViewPager) v.findViewById(R.id.vpPetImages);
+        adapter = new ImageSwipeAdapter(getActivity().getApplicationContext());
+        viewPager.setAdapter(adapter);
 
-        imgPetPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getActivity().getApplicationContext(), FullScreenImage.class);
-                imgPetPhoto.buildDrawingCache();
-                Bitmap imgPass = imgPetPhoto.getDrawingCache();
-                Bundle extras = new Bundle();
-                extras.putParcelable("petImage", imgPass);
-                i.putExtras(extras);
-                startActivity(i);
-            }
-        });
-
-        String imgURL = "https://www.pakpets.com/uploads/ad_19393335700762.jpg";
-        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
-                .cacheInMemory(true).cacheOnDisk(true).build();
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext())
-                .defaultDisplayImageOptions(defaultOptions).build();
-        ImageLoader.getInstance().init(config); // Do it on Application start
-
-        // Then later, when you want to display image
-        ImageLoader.getInstance().displayImage(imgURL, imgPetPhoto, new ImageLoadingListener() {
-            @Override
-            public void onLoadingStarted(String imageUri, View view) {
-                pbImage.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                pbImage.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                pbImage.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onLoadingCancelled(String imageUri, View view) {
-                pbImage.setVisibility(View.GONE);
-            }
-        }); // Default options will be used
+//        imgPetPhoto.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getActivity().getApplicationContext(), FullScreenImage.class);
+//                imgPetPhoto.buildDrawingCache();
+//                Bitmap imgPass = imgPetPhoto.getDrawingCache();
+//                Bundle extras = new Bundle();
+//                extras.putParcelable("petImage", imgPass);
+//                i.putExtras(extras);
+//                startActivity(i);
+//            }
+//        });
+//
+//        String imgURL = "https://www.pakpets.com/uploads/ad_19393335700762.jpg";
+//        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+//                .cacheInMemory(true).cacheOnDisk(true).build();
+//        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext())
+//                .defaultDisplayImageOptions(defaultOptions).build();
+//        ImageLoader.getInstance().init(config); // Do it on Application start
+//
+//        // Then later, when you want to display image
+//        ImageLoader.getInstance().displayImage(imgURL, imgPetPhoto, new ImageLoadingListener() {
+//            @Override
+//            public void onLoadingStarted(String imageUri, View view) {
+//                pbImage.setVisibility(View.VISIBLE);
+//            }
+//
+//            @Override
+//            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+//                pbImage.setVisibility(View.GONE);
+//            }
+//
+//            @Override
+//            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+//                pbImage.setVisibility(View.GONE);
+//            }
+//
+//            @Override
+//            public void onLoadingCancelled(String imageUri, View view) {
+//                pbImage.setVisibility(View.GONE);
+//            }
+//        }); // Default options will be used
         return v;
     } // End of onCreateView
 
